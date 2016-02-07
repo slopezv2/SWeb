63ba6f25818a065cf2d89a5e41bbd839abd6af55
Santiago Vasquez <santiagovasquez@MacBook-Air-de-Santiago.local>
2016-02-06 19:19:00 -0500

    arreglo del root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'delta/index'
+  root 'delta#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
