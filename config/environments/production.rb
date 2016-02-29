30ca040c2759a2717d7586ea033f018986dbe27d
slopezv2 <sebaslv12@hotmail.com>
2016-02-29 11:27:30 -0500

    E

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Mailer
+  config.action_mailer.delivery_method = :smtp
+# SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "idlabco.herokuapp.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['GMAIL_USERNAME'],
+    :password             => ENV['GMAIL_PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
   }
 end
