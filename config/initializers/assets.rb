2de5b1f0acf4e0967add8ccbf814166c891bb12e
slopezv2 <sebaslv12@hotmail.com>
2016-03-01 15:32:32 -0500

    s1

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.precompile += %w( jquery.scrolly.min.js )
 Rails.application.config.assets.precompile += %w( skel.min.js )
 Rails.application.config.assets.precompile += %w( util.js )
 Rails.application.config.assets.precompile += %w( main.js )
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+Rails.application.config.assets.precompile += %w( mfbold.js )
 #OA Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
