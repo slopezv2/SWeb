1b3fad996818cf3bda2639cb98074ae3bd316222
slopezv2 <sebaslv12@hotmail.com>
2016-03-19 19:02:50 -0500

    Con condicional

--- app/helpers/delta_helper.rb
+++ app/helpers/delta_helper.rb
@@ -1,2 +1,10 @@
 module DeltaHelper
+    
+    def mobile_device?
+        if session[:mobile_param]
+            return true
+        else
+            request.user_agent =~ /Mobile|webOS/
+        end
+    end
 end
