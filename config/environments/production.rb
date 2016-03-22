1dc74ccd69d150083b99f94406aec5a148cc654d
slopezv2 <sebaslv12@hotmail.com>
2016-03-22 11:52:57 -0500

    porinf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ Rails.application.configure do
   #Mailer
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
 # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
      :address              => "smtp.gmail.com",
