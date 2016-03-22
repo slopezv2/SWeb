866f71a8f713f49a828e85d58d5c46cf1f2a8c19
slopezv2 <sebaslv12@hotmail.com>
2016-03-22 11:16:42 -0500

    ds

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
      :address              => "smtp.gmail.com",
       :port                 => 587,
-      :domain               => "herokuapp.com",
+      :domain               => "heroku.com",
     :user_name            => ENV['GMAIL_USERNAME'],
     :password             => ENV['GMAIL_PASSWORD'],
     :authentication       => :plain,
